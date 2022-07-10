7e3c4dcc174a27c80391f57fe598cf9db204b201
paulomigueljs <paulomiguelblues@gmail.com>
2022-07-09 23:34:56 -0400

    Sidebar Styled

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
 import { Post } from "./Post";
-import "./global.css";
 import { Header } from "./components/Header";
+import { Sidebar } from './components/Sidebar'
+
+import styles from "./components/App.module.css";
+import "./global.css";
 
 export function App() {
   return (
     <>
       <Header />
-      <Post />
+      <div className={styles.wrapper}>
+        <Sidebar  />
+        <main>
+          <Post />
+          <Post />
+        </main>
+      </div>
     </>
   );
 }
