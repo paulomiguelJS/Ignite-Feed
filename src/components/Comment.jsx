2759e205da1eb1b9cc893d6a17b4bdbb8b7fa03d
paulomigueljs <paulomiguelblues@gmail.com>
2022-07-14 22:15:55 -0400

    Comments logic implemented

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -4,6 +4,8 @@ import { Avatar } from "./Avatar";
 import styles from "./Comment.module.css";
 
 export function Comment() {
+ 
+  
   return (
     <div className={styles.comment}>
       <Avatar hasBorder={false} src="https://github.com/paulomigueljs.png" />
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,12 +1,23 @@
 import { format, formatDistanceToNow } from "date-fns";
 import enUS from "date-fns/locale/en-US";
+import { useEffect, useState } from "react";
+
 
 import { Avatar } from "./Avatar";
 import { Comment } from "./Comment";
 
 import styles from "./Post.module.css";
 
+
+
+
+
 export function Post({ author, publishedAt, content }) {
+  const [comments, setComments] = useState([
+    1,
+    2,
+  ])
+  
   const publishedDateFormatted = format(publishedAt, "LLLL',' d 'at' h:mmaaa", {
     locale: enUS,
   });
@@ -15,6 +26,13 @@ export function Post({ author, publishedAt, content }) {
     locale: enUS,
     addSuffix: true,
   });
+
+  function handleCreateNewComment() {
+    event.preventDefault()
+
+    setComments([...comments, comments.length + 1])
+  }
+
 
   return (
     <article className={styles.post}>
@@ -48,7 +66,7 @@ export function Post({ author, publishedAt, content }) {
         })}
       </div>
 
-      <form className={styles.commentForm}>
+      <form onSubmit={handleCreateNewComment} className={styles.commentForm}>
         <strong>Leave your comment</strong>
         <textarea placeholder="Leave an comment"></textarea>
         <footer>
@@ -56,9 +74,9 @@ export function Post({ author, publishedAt, content }) {
         </footer>
       </form>
       <div className="styles.commentList">
-        <Comment />
-        <Comment />
-        <Comment />
+      {comments.map(comment => {
+        return <Comment />
+      })}
       </div>
     </article>
   );
